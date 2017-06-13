32b0659fe1dc05f9ca669242500fd253735017ee
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-06-13 20:43:02 +0300

    Introduced app assets module.

---
+++ app/assets.js
@@ -0,0 +1,13 @@
+import Loader from '../lib/loader';
+
+export default class Assets {
+  static load(callback) {
+    const resources = [{
+        name: 'tiles',
+        url: require('../static/tiles.png')
+      }
+    ];
+    const loader = new Loader(resources, this);
+    loader.load(callback);
+  }
+}

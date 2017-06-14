3c561ceb2495c2c4cbb344744cfbfe7bccc4c079
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-06-14 20:59:58 +0300

    Introduced game module.

---
+++ app/game.js
@@ -0,0 +1,30 @@
+import Assets from './assets';
+
+export default class Game {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this.ts = 0;
+    this.render = this.render.bind(this);
+  }
+
+  start() {
+    if (!this.screen) {
+      throw new Error('screen is not set');
+    }
+
+    Assets.load(() => {
+      window.requestAnimationFrame(ts => {
+        this.ts = ts;
+        this.render(ts);
+      });
+    });
+  }
+
+  render(ts) {
+    const delta = (ts - this.ts) / 1000.0;
+    this.ts = ts;
+    this.screen.render(delta);
+    window.requestAnimationFrame(this.render);
+  }
+}

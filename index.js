51dcb3f1aa3c4bf18461207a346939b861050d34
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2017-06-16 20:11:56 +0300

    Updated demo bootstrap.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+import Game from './app/game';
+import Screen from './app/screens/demo';
+
+const canvas = document.body.appendChild(document.createElement('canvas'));
+const game = new Game(canvas);
+
+game.screen = new Screen(game);
+game.start();

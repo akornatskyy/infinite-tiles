21121394dc7faf2ca1a2db1b71263748b6fe6421
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-03-05 20:13:36 +0200

    Introduced staggered map workflow.

--- app/screens/demo.js
+++ app/screens/demo.js
@@ -1,6 +1,20 @@
+import Rectangle from '../../lib/math/rectangle';
+import StaggeredMap from '../../lib/staggered-map';
+
+
+const TILE_SIZE = {
+  width: 128,
+  halfWidth: 64,
+  height: 64,
+  halfHeight: 32
+};
+
+const BOUNDS = new Rectangle(0, 0, TILE_SIZE.width * 3, TILE_SIZE.height * 3);
+
 export default class DemoScreen {
   constructor(game, debug = true) {
     this.game = game;
+    this.map = new StaggeredMap(TILE_SIZE, BOUNDS);
   }
 
   render(delta) {
@@ -9,6 +23,7 @@ export default class DemoScreen {
   }
 
   update(delta) {
+    this.map.update();
   }
 
   draw() {

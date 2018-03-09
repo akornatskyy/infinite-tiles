91739774fbdd714b9211f0670643fb94443bbea2
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2018-03-09 20:36:15 +0200

    Introduced staggered map renderer.

--- app/screens/demo.js
+++ app/screens/demo.js
@@ -1,5 +1,10 @@
 import Rectangle from '../../lib/math/rectangle';
 import StaggeredMap from '../../lib/staggered-map';
+import {
+  StaggeredMapRenderer
+} from '../../lib/staggered-map/renderer';
+
+import MapRenderer from './map-renderer';
 
 
 const TILE_SIZE = {
@@ -14,7 +19,19 @@ const BOUNDS = new Rectangle(0, 0, TILE_SIZE.width * 3, TILE_SIZE.height * 3);
 export default class DemoScreen {
   constructor(game, debug = true) {
     this.game = game;
+    game.canvas.width = BOUNDS.width;
+    game.canvas.height = BOUNDS.height;
+    if (debug) {
+      BOUNDS.left = TILE_SIZE.width + 10;
+      BOUNDS.top = TILE_SIZE.height;
+      game.canvas.width += BOUNDS.left * 2;
+      game.canvas.height += BOUNDS.top * 2;
+    }
+
     this.map = new StaggeredMap(TILE_SIZE, BOUNDS);
+    this.mapRenderer = new StaggeredMapRenderer(
+      this.map,
+      new MapRenderer(game.ctx, this.map.storage, TILE_SIZE, BOUNDS, debug));
   }
 
   render(delta) {
@@ -29,5 +46,6 @@ export default class DemoScreen {
   draw() {
     this.game.ctx.clearRect(
       0, 0, this.game.canvas.width, this.game.canvas.height);
+    this.mapRenderer.draw();
   }
 }
---
+++ app/screens/map-renderer.js
@@ -0,0 +1,51 @@
+import Assets from '../assets';
+
+export default class MapRenderer {
+  constructor(ctx, storage, tileSize, bounds, debug) {
+    this.ctx = ctx;
+    this.storage = storage;
+    this.tileSize = tileSize;
+    this.bounds = bounds;
+    this.debug = debug;
+    if (debug) {
+      this.ctx.font = '12px monospace';
+      this.ctx.fillStyle = 'white';
+      this.ctx.textAlign = 'center';
+    }
+  }
+
+  begin() {}
+
+  drawTile(tx, ty, x, y) {
+    const index = this.storage.get(tx, ty);
+    if (index === null) {
+      console.log('map renderer > draw tile: (%s:%s) is missing', tx, ty);
+      return;
+    }
+
+    this.ctx.drawImage(
+      Assets.tiles,
+      this.tileSize.width * index, 0, this.tileSize.width, this.tileSize.height,
+      x, y, this.tileSize.width, this.tileSize.height);
+
+    if (this.debug) {
+      this.ctx.fillText(
+        tx + ':' + ty,
+        x + this.tileSize.halfWidth,
+        y + this.tileSize.halfHeight + 3);
+    }
+  }
+
+  drawSelected(tx, ty, x, y) {}
+
+  end() {
+    if (this.debug) {
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = 'red';
+      this.ctx.rect(
+        this.bounds.left, this.bounds.top,
+        this.bounds.width, this.bounds.height);
+      this.ctx.stroke();
+    }
+  }
+}
